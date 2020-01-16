600c4fa3ab9f97bf103538a3431ef056942656e1
Nico Hauser <me@tyratox.ch>
2020-01-16 09:21:57 +0100

    Added curve-fitting + pca

---
+++ curve-fitting/main.cpp
@@ -0,0 +1,88 @@
+#define WITHOUT_NUMPY 1
+
+#include "../matplotlibcpp.h"
+#include <eigen3/Eigen/Dense>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+
+namespace plt = matplotlibcpp;
+
+void evaluate(VectorXd &y, const VectorXd &c, const VectorXd &x) {
+    int s = c.size();
+    int d = s - 1;
+
+    VectorXd ones = VectorXd::Ones(x.size());
+
+    y = c[s - 1] * ones;
+
+    for (int i = s - 2; i >= 0; i--) {
+        y = y.cwiseProduct(x) + c[i] * ones;
+    }
+}
+
+void polyfit(VectorXd &c, const VectorXd &x, const VectorXd &y, unsigned int d) {
+    assert(x.size() == y.size());
+    int n = x.size();
+
+    MatrixXd V(n, d + 1); //Vandermonde
+    V.col(0) = VectorXd::Ones(n);
+
+    for (int i = 1; i <= d; i++) {
+        V.col(i) = V.col(i - 1).cwiseProduct(x);
+    }
+
+    c = V.householderQr().solve(y);
+}
+
+vector<double> toCpp(VectorXd v) {
+    return vector<double>(v.data(), v.data() + v.size());
+}
+
+MatrixXd bestApproximation(const MatrixXd &A, int k) {
+    JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
+    return (svd.matrixU().leftCols(k)) * (svd.singularValues().head(k).asDiagonal()) * (svd.matrixV().leftCols(k).transpose());
+}
+
+MatrixXd getTrendVectors(const MatrixXd &A) {
+    JacobiSVD<MatrixXd> svd(A, ComputeThinU);
+    return svd.matrixU();
+}
+
+int main() {
+
+    MatrixXd B(2, 10);
+    B << -1, -0.75, -0.4, -0.3, -0.15, 0.18, 0.3, 0.6, 0.8, 1,
+        0, -0.5, -1.0, -1.1, -1.1, -1.0, -0.5, 0.1, 1.0, 2.0;
+
+    //center data around 0
+    B.row(0) = (B.row(0).array() - (B.row(0).mean())).matrix();
+    B.row(1) = (B.row(1).array() - (B.row(1).mean())).matrix();
+
+    plt::figure();
+    plt::plot(toCpp(B.row(0)), toCpp(B.row(1)), {{"label", "data points"}, {"marker", "o"}, {"linestyle", ""}});
+
+    VectorXd c;
+    polyfit(c, B.row(0), B.row(1), 2);
+
+    VectorXd samplingX = VectorXd::LinSpaced(1000, B.row(0).minCoeff(), B.row(0).maxCoeff());
+    VectorXd samplingY;
+    evaluate(samplingY, c, samplingX);
+
+    plt::plot(toCpp(samplingX), toCpp(samplingY), {{"label", "interpolated polynomial"}});
+
+    MatrixXd B1 = getTrendVectors(B);
+    for (int i = 0; i < B1.cols(); i++) {
+        c = Vector2d(0, B1(1, i) / B1(0, i));
+        evaluate(samplingY, c, samplingX);
+
+        plt::plot(toCpp(samplingX), toCpp(samplingY), {{"label", "principal component"}});
+    }
+
+    plt::legend();
+    plt::savefig("./curve-fitting.png");
+
+    return 0;
+}
